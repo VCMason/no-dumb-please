930135e2683fcf2ef067bb223c243c34b80fe979
VCMason <victorcmason@gmail.com>
2018-10-23 10:15:25 +0200

    for exp2

--- Test1.py
+++ Test1.py
@@ -3,6 +3,7 @@
 print 'This program will make a new file and output some text'
 
 print 'This is for exp1'
+print 'This is for exp2'
 
 OUT = open('outputfile.txt', 'w')
 OUT.write('Hello bioinformaticians!')
