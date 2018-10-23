fa45e2629d1555e8f5a860c8416003f7833ec90b
VCMason <victorcmason@gmail.com>
2018-10-23 11:43:03 +0200

    Please enter the commit message for your changes. Lines starting

--- Test1.py
+++ Test1.py
@@ -5,6 +5,7 @@ print 'This program will make a new file and output some text'
 print 'This is for exp1'
 print 'This is for exp2'
 print 'This is for exp3'
+print 'This is for exp3.2'
 
 OUT = open('outputfile.txt', 'w')
 OUT.write('Hello bioinformaticians!')
